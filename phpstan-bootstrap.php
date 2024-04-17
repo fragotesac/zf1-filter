eb14031b148cd3a9740f90649246f9c7f3ed48e9
Jay Klehr <jay@diablomedia.com>
2024-04-16 18:00:18 -0600

    Preparing for PHP 8.3 (#267)
    * Preparing for PHP 8.3
    
    * phpstan fix

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('MCRYPT_DEV_URANDOM')) {
+    define('MCRYPT_DEV_URANDOM', 1);
+}
+
+if (!defined('MCRYPT_DEV_RANDOM')) {
+    define('MCRYPT_DEV_RANDOM', 2);
+}
+
+if (!defined('MCRYPT_RAND')) {
+    define('MCRYPT_RAND', 3);
+}
